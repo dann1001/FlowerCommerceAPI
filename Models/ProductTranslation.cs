bc2962eb22da4cc910e6d09227c320b5a5fa4b54
Danial Irani <thederekreigns1999@gmail.com>
2024-12-28 15:33:14 +0330

    improvig localization

--- Models/ProductTranslation.cs
+++ Models/ProductTranslation.cs
@@ -1,8 +1,14 @@
 public class ProductTranslation
 {
     public int Id { get; set; }
-    public int ProductId { get; set; }  // Reference to the Product
+    public int ProductId { get; set; } // Reference to the Product
     public string Language { get; set; } // e.g., "en-US", "fa-IR"
-    public string Name { get; set; }
-    public string Description { get; set; }
+    public string Name { get; set; } = string.Empty; // Localized name
+    public string? Description { get; set; } // Localized description
+    public string? Type { get; set; } // Localized type
+    public string? Category { get; set; } // Localized category
+    public string? WaterRequirementRecommendation { get; set; }
+    public string? LightRequirementRecommendation { get; set; }
+    public string? TemperatureRequirementRecommendation { get; set; }
+    public string? SoilRequirementRecommendation { get; set; }
 }
